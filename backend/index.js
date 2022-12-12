1d8be33ab8e61b8e822e7e863f299da5115bb534
johanneslares <johannes.lares@csc.fi>
2022-12-12 14:58:13 +0200

    Add functionality

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3001
 const cors = require('cors')
+const fs = require('fs')
+const fileupload = require('express-fileupload')
 
+const uploadDir = './data'
 app.use(cors())
+app.use(fileupload({
+  useTempFiles : true,
+  tempFileDir : uploadDir
+}))
 
 app.get('/api/filelist', (req, res) => {
-  res.send([
-    {
-      name: 'hello_world.txt'
-    },
-    {
-      name: 'dummy.txt'
-    }
-  ])
+  const files = fs.readdirSync(uploadDir).map(f => {
+    return { name: f }
+  })
+  res.send(files)
 })
 
 app.get('/api/files/:file', (req, res) => {
@@ -22,6 +25,35 @@ app.get('/api/files/:file', (req, res) => {
     file,
     payload: `Some payload here from the file: ${file}`
   })
+})
+
+app.get('/api/download/:file', (req, res) => {
+  const { file } = req.params
+  fs.exists(`${uploadDir}/${file}`, function (exists) {
+    if (!exists) {
+      res.sendStatus(500)
+    }
+  })
+  res.writeHead(200, {
+    'Content-Type': 'application/octet-stream',
+    'Content-Disposition': 'attachment; filename=' + file
+  })
+  fs.createReadStream(`${uploadDir}/${file}`).pipe(res)
+  return
+})
+
+app.post('/api/files', (req, res) => {
+  try {
+    console.log(req.files.file)
+    const { name, tempFilePath } = req.files.file
+    fs.rename(tempFilePath, `${uploadDir}/${name}`, (err) => {
+      if (err) throw err
+      console.log('File saved: ', name)
+    })
+    res.send('STATUS')
+  } catch(e) {
+    res.status(500).send(e)
+  }
 })
 
 app.listen(port, () => {
--- frontend/src/components/FilePage.js
+++ frontend/src/components/FilePage.js
@@ -19,6 +19,12 @@ const FilePage = () => {
     <h1>File: {details.file}</h1>
     <div>{details.payload}</div>
     <a
+      href={`/api/download/${details.file}`}
+      download={details.file}
+      style={{ cursor: 'pointer', color: 'blue' }}
+    >Download</a>
+    <br />
+    <a
       onClick={() => navigate('/')}
       style={{ cursor: 'pointer', color: 'blue' }}
     >Back to the list</a>
